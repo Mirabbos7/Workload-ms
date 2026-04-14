20fe5ecea2b94072a1bd365ad6c294c6804171e7
Mirabbos7 <155135153+Mirabbos7@users.noreply.github.com>
2026-04-14 20:12:38 +0500

    add method to map TrainerWorkloadResponse in TrainerWorkloadMapper

--- src/main/java/org/example/workloadms/mapper/TrainerWorkloadMapper.java
+++ src/main/java/org/example/workloadms/mapper/TrainerWorkloadMapper.java
@@ -1,6 +1,7 @@
 package org.example.workloadms.mapper;
 
 import org.example.workloadms.dto.request.TrainerWorkloadRequest;
+import org.example.workloadms.dto.response.TrainerWorkloadResponse;
 import org.example.workloadms.entity.Trainer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -14,4 +15,13 @@ public interface TrainerWorkloadMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "years", ignore = true)
     Trainer toEntity(TrainerWorkloadRequest request);
+
+    default TrainerWorkloadResponse toResponse(String username, int year, int month, float workingHours) {
+        return TrainerWorkloadResponse.builder()
+                .trainerUsername(username)
+                .year(String.valueOf(year))
+                .month(String.valueOf(month))
+                .workingHours(workingHours)
+                .build();
+    }
 }
