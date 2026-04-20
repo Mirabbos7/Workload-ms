4931cd0c6d5af3a48dbacbc1a3c3bea26ea4ac1e
Mirabbos7 <155135153+Mirabbos7@users.noreply.github.com>
2026-04-21 00:40:25 +0500

    add JMS support

--- src/main/java/org/example/workloadms/WorkloadMsApplication.java
+++ src/main/java/org/example/workloadms/WorkloadMsApplication.java
@@ -2,8 +2,10 @@ package org.example.workloadms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class WorkloadMsApplication {
 
     public static void main(String[] args) {
